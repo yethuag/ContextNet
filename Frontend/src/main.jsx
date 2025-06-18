21100684ca5f87e134b7f01bfc4fa10ed1e62e93
Bambi <148939281+adorosis@users.noreply.github.com>
2025-06-18 16:40:32 +0700

    folders-template

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import {
   createBrowserRouter,
   Navigate,
@@ -8,7 +9,8 @@ import {
 } from "react-router-dom";
 import SignUp from "./pages/SignUpPage/SignUp";
 import LogIn from "./pages/LogInPage/LogIn";
-
+import AlertPage from "./pages/AlertPage/AlertPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
 const LoadingFallback = () => (
   <div className="flex justify-center items-center h-screen">
     <Loader2 className="animate-spin" size={18} />
@@ -17,6 +19,10 @@ const LoadingFallback = () => (
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element: <DashboardPage/>
+  },
+  {
     path: "/signup",
     element: <SignUp />,
   },
@@ -24,6 +30,10 @@ const router = createBrowserRouter([
     path: "/login",
     element: <LogIn />,
   },
+  {
+    path: "/alerts",
+    element: <AlertPage/>
+  }
 ]);
 
 createRoot(document.getElementById("root")).render(
